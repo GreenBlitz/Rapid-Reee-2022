53ae5f0771e71cf6ea48c440cfa49ce315ed28a2
GBSoftware Laptop <greenblitz4590@gmail.com>
2022-01-18 17:50:45 +0200

    Nitzan - added packages

--- src/main/java/edu/greenblitz/icarus/OI.java
+++ src/main/java/edu/greenblitz/icarus/OI.java
@@ -3,28 +3,28 @@ package edu.greenblitz.icarus;
 import edu.greenblitz.gblib.hid.SmartJoystick;
 
 public class OI {
-    private static OI instance;
-
-    private SmartJoystick mainJoystick;
-    private SmartJoystick secondJoystick;
-
-    private OI() {
-        mainJoystick = new SmartJoystick(RobotMap.Icarus.Joystick.MAIN);
-        secondJoystick = new SmartJoystick(RobotMap.Icarus.Joystick.SECOND);
-    }
-
-    public static OI getInstance() {
-        if (instance == null) {
-            instance = new OI();
-        }
-        return instance;
-    }
-
-    public SmartJoystick getMainJoystick() {
-        return mainJoystick;
-    }
-
-    public SmartJoystick getSecondJoystick() {
-        return secondJoystick;
-    }
+	private static OI instance;
+	
+	private SmartJoystick mainJoystick;
+	private SmartJoystick secondJoystick;
+	
+	private OI() {
+		mainJoystick = new SmartJoystick(RobotMap.Icarus.Joystick.MAIN);
+		secondJoystick = new SmartJoystick(RobotMap.Icarus.Joystick.SECOND);
+	}
+	
+	public static OI getInstance() {
+		if (instance == null) {
+			instance = new OI();
+		}
+		return instance;
+	}
+	
+	public SmartJoystick getMainJoystick() {
+		return mainJoystick;
+	}
+	
+	public SmartJoystick getSecondJoystick() {
+		return secondJoystick;
+	}
 }
