cbde22f99de0cf9e377dc2c4835a1871d4b336f7
Raz Rishoni <raz@Razs-MacBook-Air.local>
2022-01-19 10:03:25 +0200

    Raz - added roller commands

--- src/main/java/edu/greenblitz/icarus/commands/intake/roller/RollerCommand.java
+++ src/main/java/edu/greenblitz/icarus/commands/intake/roller/RollerCommand.java
@@ -1,4 +1,6 @@
-package edu.greenblitz.icarus.commands.intake;
+package edu.greenblitz.icarus.commands.intake.roller;
+
+import edu.greenblitz.icarus.commands.intake.IntakeCommand;
 
 public abstract class RollerCommand extends IntakeCommand {
     public RollerCommand() {
