3450f6915004a8b59b24fc0780bc9eb2a5b16228
unknown <greenblitz.4590@gmail.com>
2022-01-28 01:16:17 +0200

    Nitzan - copied & refactored Chassis code from Infinite Reee, probably needs more work

--- src/main/java/edu/greenblitz/icarus/RobotMap.java
+++ src/main/java/edu/greenblitz/icarus/RobotMap.java
@@ -9,25 +9,31 @@ public class RobotMap {
 			public static final int MAIN = 0;
 			public static final int SECOND = 1;
 		}
-		
+
 		public static class Chassis {
-			public static class Motor {
-				public static final int LEFT_VICTOR = 2,
-						RIGHT_VICTOR = 3,
-						LEFT_TALON = 1,
-						RIGHT_TALON = 4;
+			public static final double WHEEL_DIST = 0.0; //very accurate right now
+
+			public static class Motors {
+				public static final int RIGHT_LEADER = -1, RIGHT_FOLLOWER_1 = -1, RIGHT_FOLLOWER_2 = -1, LEFT_LEADER = -1, LEFT_FOLLOWER_1 = -1, LEFT_FOLLOWER_2 = -1;
+				public static final boolean RIGHT_LEADER_REVERSED = false, RIGHT_FOLLOWER_1_REVERSED = false, RIGHT_FOLLOWER_2_REVERSED = false, LEFT_LEADER_REVERSED = false, LEFT_FOLLOWER_1_REVERSED = false, LEFT_FOLLOWER_2_REVERSED = false;
+			}
+
+			public static class Encoders {
+				public static final GearDependentValue<Double> NORM_CONST_SPARK = new GearDependentValue<>(2300.0 * 0.64, 1234.0 / 2.0); // TODO: check this, I copied it from Infinite Reeee
+				public static final int RIGHT_ENCODER = -1, LEFT_ENCODER = -1;
+				public static final boolean RIGHT_ENCODER_REVERSED = false, LEFT_ENCODER_REVERSED = false;
 			}
 		}
 
-		public static class Intake{
+		public static class Intake {
 			public static final int PCM = -1;
 
-			public static class Motors{
+			public static class Motors {
 				public static final int ROLLER_PORT = -1;
 				public static final boolean IS_REVERSED = false;
 			}
 
-			public static class Solenoid{
+			public static class Solenoid {
 				public static final int FORWARD_LEFT = -1;
 				public static final int REVERSE_LEFT = -1;
 				public static final int FORWARD_RIGHT = -1;
@@ -45,13 +51,12 @@ public class RobotMap {
 
 		public static class Shooter {
 			public static class ShooterMotor {
-				public static final int PORT_LEADER = 8,
-						PORT_FOLLOWER = 9;
-				public static final boolean LEADER_INVERTED = false,
-						FOLLOWER_INVERTED = false;
+				public static final int PORT_LEADER = 8, PORT_FOLLOWER = 9;
+				public static final boolean LEADER_INVERTED = false, FOLLOWER_INVERTED = false;
 
 
 				public static final Dataset RPM_TO_POWER = new Dataset(2);
+
 				static {
 					RPM_TO_POWER.addDatapoint(0, new double[]{0});
 					RPM_TO_POWER.addDatapoint(975, new double[]{0.2});
