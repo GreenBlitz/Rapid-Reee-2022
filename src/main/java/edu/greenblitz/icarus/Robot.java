02574e3c72cb2e27228ae04bc671b7bc904e6bcc
unknown <greenblitz.4590@gmail.com>
2022-01-28 00:36:22 +0200

    Nitzan - added safety feature so that no one kills another battery

--- src/main/java/edu/greenblitz/icarus/Robot.java
+++ src/main/java/edu/greenblitz/icarus/Robot.java
@@ -1,21 +1,25 @@
 package edu.greenblitz.icarus;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
-	
+
 	@Override
 	public void robotInit() {
 		OI.getInstance();
 		CommandScheduler.getInstance().enable();
 	}
-	
+
 	@Override
 	public void robotPeriodic() {
 		CommandScheduler.getInstance().run();
+		if (RobotController.getBatteryVoltage() < 11) {
+			System.err.println("BATTERY LEVELS DANGEROUSLY LOW");
+		}
 	}
-	
+
 	@Override
 	public void teleopInit(){    }
 }
