165c48d929e5627a0ab13a8a186311e66eab94b6
GBSoftwareLaptop <you@example.com>
2022-02-01 17:33:31 +0200

    Nitzan - made FunnelCommand abstract and reformatted

--- src/main/java/edu/greenblitz/icarus/commands/funnel/FunnelCommand.java
+++ src/main/java/edu/greenblitz/icarus/commands/funnel/FunnelCommand.java
@@ -3,11 +3,12 @@ package edu.greenblitz.icarus.commands.funnel;
 import edu.greenblitz.gblib.command.GBCommand;
 import edu.greenblitz.icarus.subsystems.Funnel;
 
-public class FunnelCommand extends GBCommand {
-	
+public abstract class FunnelCommand extends GBCommand {
+
 	protected Funnel funnel;
-	
+
 	public FunnelCommand() {
 		funnel = Funnel.getInstance();
+		require(funnel);
 	}
 }
--- src/main/java/edu/greenblitz/icarus/commands/funnel/InsertIntoShooter.java
+++ src/main/java/edu/greenblitz/icarus/commands/funnel/InsertIntoShooter.java
@@ -1,6 +1,5 @@
 package edu.greenblitz.icarus.commands.funnel;
 
-import edu.greenblitz.icarus.commands.funnel.PushByConstant;
 import edu.greenblitz.icarus.commands.intake.roller.RollByConstant;
 import edu.greenblitz.icarus.subsystems.Shooter;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
@@ -9,20 +8,20 @@ import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 
 public class InsertIntoShooter extends SequentialCommandGroup {
 
-    public InsertIntoShooter(double pushConst, double rollConst) {
+	public InsertIntoShooter(double pushConst, double rollConst) {
 
-        addCommands(
+		addCommands(
 
-                new WaitUntilCommand(() -> Shooter.getInstance().isPreparedToShoot()),
-                new ParallelCommandGroup(
+				new WaitUntilCommand(() -> Shooter.getInstance().isPreparedToShoot()),
+				new ParallelCommandGroup(
 
-                        new PushByConstant(pushConst),
-                        new RollByConstant(rollConst)
+						new PushByConstant(pushConst),
+						new RollByConstant(rollConst)
 
-                )
+				)
 
-        );
+		);
 
-    }
+	}
 
 }
