45e03c4aef0fc9043022f873497e8da91aedf9ac
GBSoftwareLaptop <you@example.com>
2022-02-01 17:33:31 +0200

    Nitzan - made FunnelCommand abstract and reformatted

--- src/main/java/edu/greenblitz/icarus/commands/funnel/PushByConstant.java
+++ src/main/java/edu/greenblitz/icarus/commands/funnel/PushByConstant.java
@@ -2,25 +2,25 @@ package edu.greenblitz.icarus.commands.funnel;
 
 public class PushByConstant extends FunnelCommand {
 
-    private double power;
+	private double power;
 
-    public PushByConstant(double power) {
-        super();
-        this.power = power;
-    }
+	public PushByConstant(double power) {
+		super();
+		this.power = power;
+	}
 
-    @Override
-    public void execute() {
-        funnel.moveMotor(power);
-    }
+	@Override
+	public void execute() {
+		funnel.moveMotor(power);
+	}
 
-    @Override
-    public void end(boolean interrupted) {
-        funnel.moveMotor(0);
-    }
+	@Override
+	public void end(boolean interrupted) {
+		funnel.moveMotor(0);
+	}
 
-    @Override
-    public boolean isFinished() {
-        return false;
-    }
+	@Override
+	public boolean isFinished() {
+		return false;
+	}
 }
