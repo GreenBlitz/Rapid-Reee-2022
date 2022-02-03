4807655d43a92caa3d32958ed635410566693436
unknown <greenblitz.4590@gmail.com>
2022-02-03 21:27:12 +0200

    WiFi - Added PID commands

---
+++ src/main/java/edu/greenblitz/icarus/commands/complexClimb/HookMotorByPID.java
@@ -0,0 +1,22 @@
+package edu.greenblitz.icarus.commands.complexClimb;
+
+import org.greenblitz.motion.pid.PIDController;
+import org.greenblitz.motion.pid.PIDObject;
+
+public class HookMotorByPID extends ComplexClimbCommand {
+	private PIDController controller;
+
+	public HookMotorByPID(PIDObject obj) {
+		controller = new PIDController(obj);
+	}
+
+	@Override
+	public void execute() {
+		complexClimb.safeMoveHookMotor(controller.calculatePID(-1)); // TODO: Check for PID value
+	}
+
+	@Override
+	public void end(boolean interrupted) {
+		complexClimb.safeMoveHookMotor(0);
+	}
+}
