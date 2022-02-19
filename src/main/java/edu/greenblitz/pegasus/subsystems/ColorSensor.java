f92c3348c4a5eaa7804abdd63c117c8744bcde28
Nitzan Bresler <bresler.nitzan@gmail.com>
2022-02-19 23:36:04 +0200

    Nitzan - stop copying code from the internet

--- src/main/java/edu/greenblitz/pegasus/subsystems/ColorSensor.java
+++ src/main/java/edu/greenblitz/pegasus/subsystems/ColorSensor.java
@@ -4,12 +4,12 @@ import com.revrobotics.ColorSensorV3;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.util.Color;
 
-public class ColorSensor extends GBSubsystem{
+public class ColorSensor extends GBSubsystem {
 	private static ColorSensor instance;
 	private final I2C.Port i2cPort = I2C.Port.kOnboard;
-	private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+	private final ColorSensorV3 colorSensor = new ColorSensorV3(i2cPort);
 
-	private ColorSensor(){
+	private ColorSensor() {
 
 	}
 
@@ -24,7 +24,7 @@ public class ColorSensor extends GBSubsystem{
 		return instance;
 	}
 
-	public Color getColor(){
-		return m_colorSensor.getColor();
+	public Color getColor() {
+		return colorSensor.getColor();
 	}
 }
