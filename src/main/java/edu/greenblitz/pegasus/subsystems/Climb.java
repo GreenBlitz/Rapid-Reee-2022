4a2ed64b37dfcceab27b243de4ad59fdbbbd38f1
Nitzan <bresler.nitzan@gmail.com>
2022-03-02 21:17:58 +0200

    Nitzan - minor fix for climb logic

--- src/main/java/edu/greenblitz/pegasus/subsystems/Climb.java
+++ src/main/java/edu/greenblitz/pegasus/subsystems/Climb.java
@@ -6,46 +6,49 @@ import edu.greenblitz.gblib.encoder.SparkEncoder;
 import edu.greenblitz.pegasus.RobotMap;
 
 public class Climb extends GBSubsystem {
-    private static Climb instance;
-    private CANSparkMax motor;
-    private SparkEncoder encoder;
+	private static Climb instance;
+	private CANSparkMax motor;
+	private SparkEncoder encoder;
 
-    private Climb() {
-        motor = new CANSparkMax(RobotMap.Pegasus.Climb.Motor.MOTOR, CANSparkMaxLowLevel.MotorType.kBrushless);
-        motor.setInverted(RobotMap.Pegasus.Climb.Motor.MOTOR_REVERSE);
-        encoder = new SparkEncoder(RobotMap.Pegasus.Climb.Motor.MOTOR_TICKS_PER_METER, motor);
-    }
+	private Climb() {
+		motor = new CANSparkMax(RobotMap.Pegasus.Climb.Motor.MOTOR, CANSparkMaxLowLevel.MotorType.kBrushless);
+		motor.setInverted(RobotMap.Pegasus.Climb.Motor.MOTOR_REVERSE);
+		encoder = new SparkEncoder(RobotMap.Pegasus.Climb.Motor.MOTOR_TICKS_PER_METER, motor);
+	}
 
-    @Override
-    public void periodic() {
-        super.periodic();
-        //Add data to smartdashboard here
-    }
+	@Override
+	public void periodic() {
+		super.periodic();
+		//Add data to smartdashboard here
+	}
 
-    private static void init() {
-        instance = new Climb();
-    }
+	private static void init() {
+		instance = new Climb();
+	}
 
-    public static Climb getInstance() {
-        if (instance == null) {
-            init();
-        }
-        return instance;
-    }
+	public static Climb getInstance() {
+		if (instance == null) {
+			init();
+		}
+		return instance;
+	}
 
-    private void moveMotor(double power) {
-        motor.set(power);
-    }
+	private void moveMotor(double power) {
+		motor.set(power);
+	}
 
-    public void safeMoveMotor(double power) {
-        if (power < 0 || power > 1) {
-            moveMotor(0);
-            return;
-        }
-        moveMotor(power);
-    }
+	public void safeMoveMotor(double power) {
+		if (power < 0) {
+			moveMotor(0);
+			return;
+		} else if (power > 1) {
+			moveMotor(1);
+			return;
+		}
+		moveMotor(power);
+	}
 
-    public double getMotorTicks() {
-        return encoder.getRawTicks();
-    }
+	public double getMotorTicks() {
+		return encoder.getRawTicks();
+	}
 }
