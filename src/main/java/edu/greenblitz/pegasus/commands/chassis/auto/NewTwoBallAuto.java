d74e8cd5e8748a939179a3689baa4eb293a80978
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2022-03-05 16:23:28 +0200

    Raz - Ctrl Alt L

--- src/main/java/edu/greenblitz/pegasus/commands/chassis/auto/NewTwoBallAuto.java
+++ src/main/java/edu/greenblitz/pegasus/commands/chassis/auto/NewTwoBallAuto.java
@@ -8,6 +8,12 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 public class NewTwoBallAuto extends SequentialCommandGroup {
 
 	public NewTwoBallAuto(){
-		addCommands(new ParallelCommandGroup(new MoveSimpleByPID(-2),new HandleBalls()),new MoveSimpleByPID(4),new DoubleShoot());
+		addCommands(
+				new ParallelCommandGroup(
+						new MoveSimpleByPID(-2),
+						new HandleBalls()
+				),
+				new MoveSimpleByPID(4),new DoubleShoot()
+		);
 	}
 }
