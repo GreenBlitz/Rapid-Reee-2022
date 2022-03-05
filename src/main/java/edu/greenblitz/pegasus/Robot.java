9b1e1ff8a2eb594ac922bb285e88599e563f944e
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2022-03-05 16:17:37 +0200

    Raz - made code suck less

--- src/main/java/edu/greenblitz/pegasus/Robot.java
+++ src/main/java/edu/greenblitz/pegasus/Robot.java
@@ -1,6 +1,7 @@
 package edu.greenblitz.pegasus;
 
 import edu.greenblitz.pegasus.commands.chassis.auto.ShootAndGo;
+import edu.greenblitz.pegasus.commands.indexing.HandleBalls;
 import edu.greenblitz.pegasus.commands.climb.ClimbByJoysticks;
 import edu.greenblitz.pegasus.subsystems.*;
 import edu.greenblitz.pegasus.utils.DigitalInputMap;
@@ -12,41 +13,40 @@ public class Robot extends TimedRobot {
 	public void robotInit() {
 		CommandScheduler.getInstance().enable();
 		DigitalInputMap.getInstance();
-		Pneumatics.getInstance();
 		Intake.getInstance();
 		Shifter.getInstance();
 		Funnel.getInstance();
 		Shooter.init();
-		Climb.getInstance();
+		Climb.getInstance().setDefaultCommand(new ClimbByJoysticks(OI.getInstance().getMainJoystick()));
 		OI.getInstance();
-		Indexing.getInstance();
+		Indexing.getInstance().setDefaultCommand(new HandleBalls());
 //      VisionMaster.getInstance().register();
 //		ColorSensor.getInstance();
 		Chassis.getInstance(); // Must be last!
 	}
-
+	
 	@Override
 	public void robotPeriodic() {
 		CommandScheduler.getInstance().run();
 	}
-
+	
 	@Override
 	public void disabledInit() {
 		//VisionMaster.GameState.DISABLED.setAsCurrent();
 		CommandScheduler.getInstance().cancelAll();
 	}
-
+	
 	@Override
 	public void teleopInit() {
 		CommandScheduler.getInstance().cancelAll();
 		Chassis.getInstance().toCoast();
 	}
-
+	
 	@Override
 	public void teleopPeriodic() {
 	}
-
-
+	
+	
 	@Override
 	public void autonomousInit() {
 
@@ -57,16 +57,16 @@ public class Robot extends TimedRobot {
 				new RobotDotMove(-0.1) //auto line in the back of the robot
 		).schedule();
 		 */
-
+		
 		new ShootAndGo(5).schedule(); //Shoot and go
 //		new FourBallAuto(0.3).schedule(); // 2 ball auto
 	}
-
+	
 	@Override
 	public void testInit() {
 		CommandScheduler.getInstance().cancelAll();
 	}
-
+	
 	@Override
 	public void testPeriodic() {
 	}
