8fc2f22e11123cde4d5c53852edcf311b6b39521
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2022-03-20 00:00:15 +0200

    Raz & Nitzan - now maintains speed over auto

--- src/main/java/edu/greenblitz/pegasus/commands/auto/FourBallAuto.java
+++ src/main/java/edu/greenblitz/pegasus/commands/auto/FourBallAuto.java
@@ -1,17 +1,20 @@
 package edu.greenblitz.pegasus.commands.auto;
 
+import edu.greenblitz.pegasus.RobotMap;
 import edu.greenblitz.pegasus.commands.climb.ToggleClimbPosition;
 import edu.greenblitz.pegasus.commands.intake.extender.ExtendRoller;
 import edu.greenblitz.pegasus.commands.intake.roller.RunRoller;
 import edu.greenblitz.pegasus.commands.multiSystem.MoveBallUntilClick;
 import edu.greenblitz.pegasus.commands.shifter.ToPower;
 import edu.greenblitz.pegasus.commands.shooter.DoubleShoot;
+import edu.greenblitz.pegasus.commands.shooter.ShooterByRPM;
+import edu.greenblitz.pegasus.commands.shooter.ShooterCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import org.greenblitz.motion.pid.PIDObject;
 
-public class FourBallAuto extends SequentialCommandGroup {
+public class FourBallAuto extends ParallelCommandGroup {
 	private static final double FIRST_DISTANCE = 0.0;
 	private static final double SECOND_DISTANCE = 0.0;
 	private static final double ANGLE = 0.0;
@@ -22,27 +25,30 @@ public class FourBallAuto extends SequentialCommandGroup {
 
 	public FourBallAuto(){
 		addCommands(
-				new ToPower(),
-				new ExtendRoller(),
-				new ParallelCommandGroup(
-						new MoveFunnelUntilClick(),
-						new RunRoller(),
-						new InstantCommand(new ToggleClimbPosition()),
-						new MoveLinearByPID(LIN_OBJECT, FIRST_DISTANCE)
-				),
-				new DoubleShoot(5100),
-				new MoveAngleByPID(ANG_OBJECT, ANGLE),
-				new ParallelCommandGroup(
-						new MoveFunnelUntilClick(),
-						new RunRoller(),
-						new MoveLinearByPID(LIN_OBJECT, SECOND_DISTANCE)
-				),
-				new ParallelCommandGroup(
-						new MoveBallUntilClick(),
-						new MoveLinearByPID(LIN_OBJECT, -SECOND_DISTANCE)
-				),
-				new MoveAngleByPID(ANG_OBJECT, -ANGLE),
-				new DoubleShoot(5100)
+				new ShooterByRPM(RobotMap.Pegasus.Shooter.ShooterMotor.pid, RobotMap.Pegasus.Shooter.ShooterMotor.iZone, 5100),
+				new SequentialCommandGroup(
+					new ToPower(),
+					new ExtendRoller(),
+					new ParallelCommandGroup(
+							new MoveFunnelUntilClick(),
+							new RunRoller(),
+							new InstantCommand(new ToggleClimbPosition()),
+							new MoveLinearByPID(LIN_OBJECT, FIRST_DISTANCE)
+					),
+					new DoubleShoot(5100),
+					new MoveAngleByPID(ANG_OBJECT, ANGLE),
+					new ParallelCommandGroup(
+							new MoveFunnelUntilClick(),
+							new RunRoller(),
+							new MoveLinearByPID(LIN_OBJECT, SECOND_DISTANCE)
+					),
+					new ParallelCommandGroup(
+							new MoveBallUntilClick(),
+							new MoveLinearByPID(LIN_OBJECT, -SECOND_DISTANCE)
+					),
+					new MoveAngleByPID(ANG_OBJECT, -ANGLE),
+					new DoubleShoot(5100)
+				)
 		);
 	}
 }
