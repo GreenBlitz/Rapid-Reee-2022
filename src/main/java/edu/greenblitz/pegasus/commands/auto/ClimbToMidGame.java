fde4a63a5bfc66efcc6f560b925729907610a041
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2022-03-27 10:23:44 +0300

    Raz - 3 ball prep

--- src/main/java/edu/greenblitz/pegasus/commands/auto/ClimbToMidGame.java
+++ src/main/java/edu/greenblitz/pegasus/commands/auto/ClimbToMidGame.java
@@ -1,2 +1,19 @@
-package edu.greenblitz.pegasus.commands.auto;public class ClimbToMidGame {
+package edu.greenblitz.pegasus.commands.auto;
+
+import edu.greenblitz.pegasus.commands.climb.ClimbMoveToPosition;
+import edu.greenblitz.pegasus.commands.climb.ClimbState;
+import edu.greenblitz.pegasus.commands.climb.Rail.MoveRailToPosition;
+import edu.greenblitz.pegasus.commands.climb.Turning.MoveTurningToAngle;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+import static edu.greenblitz.pegasus.RobotMap.Pegasus.Climb.ClimbMotors.MID_START_ANGLE;
+
+public class ClimbToMidGame extends SequentialCommandGroup {
+	public ClimbToMidGame(){
+		addCommands(
+			new MoveRailToPosition(0.613),
+			new MoveTurningToAngle(MID_START_ANGLE),
+			new ClimbMoveToPosition(ClimbState.MID_GAME)
+		);
+	}
 }
--- src/main/java/edu/greenblitz/pegasus/commands/auto/ThreeBallAuto.java
+++ src/main/java/edu/greenblitz/pegasus/commands/auto/ThreeBallAuto.java
@@ -1,2 +1,41 @@
-package edu.greenblitz.pegasus.commands.auto;public class ThreeBallAuto {
+package edu.greenblitz.pegasus.commands.auto;
+
+import edu.greenblitz.pegasus.RobotMap;
+import edu.greenblitz.pegasus.commands.intake.extender.ExtendRoller;
+import edu.greenblitz.pegasus.commands.intake.roller.RunRoller;
+import edu.greenblitz.pegasus.commands.shifter.ToSpeed;
+import edu.greenblitz.pegasus.commands.shooter.ShooterByRPM;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+import static edu.greenblitz.pegasus.commands.auto.FourBallAuto.LIN_OBJECT;
+import static edu.greenblitz.pegasus.commands.auto.FourBallAuto.LIN_OBJECT_ANG;
+
+public class ThreeBallAuto extends SequentialCommandGroup {
+	public ThreeBallAuto(){
+		addCommands(
+				// Open roller and prepare to go to the first ball
+				new ExtendRoller(),
+				new ClimbToMidGame(),
+				new ToSpeed(),
+
+				// Go to the first
+				new ParallelDeadlineGroup(
+						new ParallelCommandGroup(
+								new MoveFunnelUntilClick(),
+								new SequentialCommandGroup(
+										new MoveLinearByPID(LIN_OBJECT, LIN_OBJECT_ANG, FIRST_DISTANCE),
+										new WaitCommand(0.2)
+								)
+						),
+						new RunRoller(),
+						new SequentialCommandGroup(
+								new WaitCommand(0.5),
+								new ShooterByRPM(RobotMap.Pegasus.Shooter.ShooterMotor.pid, RobotMap.Pegasus.Shooter.ShooterMotor.iZone, RobotMap.Pegasus.Shooter.ShooterMotor.RPM)
+						)
+				),
+		);
+	}
 }
