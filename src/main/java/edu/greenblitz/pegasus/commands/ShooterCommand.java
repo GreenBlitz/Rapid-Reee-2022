9dea3c8cf10a358a6ec8edb7e6f6bd0f2a31ea34
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2022-04-20 16:08:58 +0300

    Amir Gutman & Tal - added Shooter subsystem and 3 new Commands ShootByPower,ShootByTrigger and ToggleShooter. Also added Shooter Command

---
+++ src/main/java/edu/greenblitz/pegasus/commands/ShooterCommand.java
@@ -0,0 +1,2 @@
+package edu.greenblitz.pegasus.commands;public class ShooterCommand {
+}
--- src/main/java/edu/greenblitz/pegasus/commands/climb/ClimbState.java
+++
@@ -1,5 +0,0 @@
-package edu.greenblitz.pegasus.commands.climb;
-
-public enum ClimbState {
-		PULL_UP, TRAVERSE, MID_GAME, START, GAME_INIT
-}
