9c9e34c4a609b5eb1e7b1002e04bbf8a00de7bd8
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2022-08-16 19:58:57 +0300

    noam & romy - added ff to ShooterByRPM, TESTED

--- src/main/java/edu/greenblitz/pegasus/commands/shooter/ShooterByRPM.java
+++ src/main/java/edu/greenblitz/pegasus/commands/shooter/ShooterByRPM.java
@@ -1,19 +1,19 @@
 package edu.greenblitz.pegasus.commands.shooter;
 
 import edu.greenblitz.gblib.motion.pid.PIDObject;
+import edu.greenblitz.pegasus.RobotMap;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ShooterByRPM extends ShooterCommand {
 	private static final double EPSILON = 50;
-	protected PIDObject obj;
-	protected double iZone;
+	protected PIDObject pidObject;
 	//	protected RemoteCSVTarget logger;
 	protected double target;
 	protected double tStart;
 	private int inShootingSpeed;
 	
-	public ShooterByRPM(PIDObject obj, double target) {
-		this.obj = obj;
+	public ShooterByRPM(PIDObject pidObject, double target) {
+		this.pidObject = pidObject;
 		this.target = target;
 //		this.logger = RemoteCSVTarget.initTarget("FlyWheelVel", "time", "vel");
 		this.inShootingSpeed = 0;
@@ -24,7 +24,9 @@ public class ShooterByRPM extends ShooterCommand {
 	public void initialize() {
 		shooter.setPreparedToShoot(false);
 //		shooter.getPIDController().setIAccum(0.0);
-		shooter.setPIDConsts(obj);
+		PIDObject temp = new PIDObject(pidObject);
+		temp.setFF(RobotMap.Pegasus.Shooter.ShooterMotor.RPM_TO_POWER.linearlyInterpolate(target)[0] / target);
+		shooter.setPIDConsts(temp);
 		tStart = System.currentTimeMillis() / 1000.0;
 	}
 	
@@ -35,10 +37,8 @@ public class ShooterByRPM extends ShooterCommand {
 		SmartDashboard.putNumber("inShootingSpeed", inShootingSpeed);
 		SmartDashboard.putNumber("RPM", shooter.getShooterSpeed());
 
-
 		if (Math.abs(target - shooter.getShooterSpeed()) < EPSILON) {
 			this.inShootingSpeed++;
-			
 		} else {
 			this.inShootingSpeed = 0;
 			shooter.setPreparedToShoot(false);
@@ -56,7 +56,7 @@ public class ShooterByRPM extends ShooterCommand {
 	
 	@Override
 	public void end(boolean interrupted) {
-		
+		System.out.println("finished");
 		shooter.setPreparedToShoot(false);
 		this.inShootingSpeed = 0;
 		super.end(interrupted);
