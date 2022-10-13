d79f4d3c50d4b163e3cef9769858491820ab6d39
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2022-10-13 08:08:04 +0300

    Tal & Nitzan - copied to 2nd laptop

--- src/main/java/edu/greenblitz/pegasus/commands/swerve/CombineJoystickMovement.java
+++ src/main/java/edu/greenblitz/pegasus/commands/swerve/CombineJoystickMovement.java
@@ -31,7 +31,7 @@ public class CombineJoystickMovement extends SwerveCommand {
 	public void execute() {
 		double rightwardSpeed = -joystick.getAxisValue(SmartJoystick.Axis.LEFT_X) * LIN_SPEED_FACTOR;
 		double forwardSpeed = joystick.getAxisValue(SmartJoystick.Axis.LEFT_Y) * LIN_SPEED_FACTOR;
-		double angSpeed = -joystick.getAxisValue(SmartJoystick.Axis.RIGHT_X) * ANG_SPEED_FACTOR;
+		double angSpeed = joystick.getAxisValue(SmartJoystick.Axis.RIGHT_X) * ANG_SPEED_FACTOR;
 		if (forwardSpeed == 0 && rightwardSpeed == 0 && angSpeed == 0) {
 			swerve.stop();
 			return;
