17e3b057447a249aded4d5fdf40201718897d77a
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2022-11-21 11:03:57 +0200

    added a tick timer until disables the robot when the battery goes brrrrrr

--- src/main/java/edu/greenblitz/pegasus/commands/BatteryDisabler.java
+++ src/main/java/edu/greenblitz/pegasus/commands/BatteryDisabler.java
@@ -2,12 +2,15 @@ package edu.greenblitz.pegasus.commands;
 
 import edu.greenblitz.GBLib.src.main.java.edu.greenblitz.gblib.base.GBCommand;
 import edu.greenblitz.pegasus.subsystems.Battery;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class BatteryDisabler extends GBCommand {
 
-	private boolean under;
 	private Battery battery;
+	public static final int disableAfterTicks = 5;
+	public int timesUnder;
+
 
 	public BatteryDisabler (){
 		battery = new Battery();
@@ -16,15 +19,18 @@ public class BatteryDisabler extends GBCommand {
 
 	@Override
 	public void initialize() {
-		under = false;
+		timesUnder = 0;
+		CommandScheduler.getInstance().enable(); //todo - is this need?
 	}
 
 	@Override
 	public void execute() {
 		if(battery.getCurrentVoltage() < battery.getMinVoltage()){
-			under = true;
+			timesUnder++;
 		}
-		if (under){
+		if (timesUnder >= disableAfterTicks &&
+				DriverStation.getMatchType() == DriverStation.MatchType.None ||
+				DriverStation.getMatchType() == DriverStation.MatchType.Practice){
 			CommandScheduler.getInstance().cancelAll();
 			CommandScheduler.getInstance().disable();
 		}
