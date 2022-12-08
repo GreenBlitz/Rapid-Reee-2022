74a8c5c3673c7ec334622c0444b0b73007aa6b04
NoamRosenberg08 <noam.rosenberg.private@gmail.com>
2022-12-08 21:23:59 +0200

    added comments and missing return statements to some of the SDS Swerve module functions

--- src/main/java/edu/greenblitz/pegasus/subsystems/swerve/SdsSwerveModule.java
+++ src/main/java/edu/greenblitz/pegasus/subsystems/swerve/SdsSwerveModule.java
@@ -14,7 +14,6 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.AnalogInput;
 
 public class SdsSwerveModule implements SwerveModule{
-
 	public double targetAngle;
 	public double targetVel;
 	private TalonFX angleMotor;
@@ -37,14 +36,14 @@ public class SdsSwerveModule implements SwerveModule{
 		linearMotor = new TalonFX(linearMotorID);
 		linearMotor.configSupplyCurrentLimit(
 				new SupplyCurrentLimitConfiguration(
-						true,30,30,0));;
+						true,30,30,0));
 		linearMotor.configClosedloopRamp(0.4);
 		linearMotor.configOpenloopRamp(0.4);
 		linearMotor.setInverted(linInverted);
 		linearMotor.configSelectedFeedbackCoefficient(RobotMap.Pegasus.Swerve.linTicksToMeters);
 
 		magEncoder = new AnalogInput(lampreyID);
-		magEncoder.setAverageBits(2);
+		magEncoder.setAverageBits(2); //todo find real bits
 		configAnglePID(RobotMap.Pegasus.Swerve.angPID);
 		configLinPID(RobotMap.Pegasus.Swerve.linPID);
 		this.feedforward = new SimpleMotorFeedforward(RobotMap.Pegasus.Swerve.ks, RobotMap.Pegasus.Swerve.kv, RobotMap.Pegasus.Swerve.ka);;
@@ -52,13 +51,18 @@ public class SdsSwerveModule implements SwerveModule{
 	}
 
 
+	/**
+	 * @param moduleState - @class {@link SwerveModuleState} to set the module to (angle and velocity)
+	 */
 	@Override
 	public void setModuleState(SwerveModuleState moduleState) {
 		setLinSpeed(moduleState.speedMetersPerSecond);
 		rotateToAngle(moduleState.angle.getRadians());
-
 	}
 
+	/**
+	 * @param angle - angle to turn to in radians
+	 */
 	@Override
 	public void rotateToAngle(double angle) {
 		double diff = GBMath.modulo(angle - getModuleAngle(), 2 * Math.PI);
@@ -66,27 +70,38 @@ public class SdsSwerveModule implements SwerveModule{
 		angle = getModuleAngle() + diff;
 
 		angleMotor.set(ControlMode.Position,angle);
-//			.setReference(angle, CANSparkMax.ControlType.kPosition);
 
 		targetAngle = angle;
 	}
 
+	/**
+	 * @return get the module angle in radians
+	 */
 	@Override
 	//maybe not after gear ratio plz check ~ noam
 	public double getModuleAngle() {
 		return angleMotor.getSelectedSensorPosition();
 	}
 
+	/**
+	 * @return returns the current linear motor velocity
+	 */
 	@Override
 	public double getCurrentVelocity() {
 		return linearMotor.getSelectedSensorVelocity();
 	}
 
+	/**
+	 * @param angle - Position to set for the angular encoder (in raw sensor units).
+	 */
 	@Override
 	public void resetEncoderToValue(double angle) {
 		angleMotor.setSelectedSensorPosition(angle);
 	}
 
+	/**
+	 * sets the encoder current position to 0;
+	 */
 	@Override
 	public void resetEncoderToValue() {
 		angleMotor.setSelectedSensorPosition(0);
@@ -110,9 +125,11 @@ public class SdsSwerveModule implements SwerveModule{
 		angleMotor.config_kF(0,pidObject.getKf());
 		angleMotor.config_IntegralZone(0,pidObject.getIZone());
 		angleMotor.configClosedLoopPeakOutput(0,pidObject.getMaxPower());
-
 	}
 
+	/**
+	 * @param speed - double of the wanted speed of the linear motor, uses PID and feedForward
+	 */
 	@Override
 	public void setLinSpeed(double speed) {
 		linearMotor.set(
@@ -122,22 +139,34 @@ public class SdsSwerveModule implements SwerveModule{
 				feedforward.calculate(speed));
 	}
 
+	/**
+	 * stops the angular and linear module.
+	 */
 	@Override
 	public void stop() {
 		linearMotor.set(ControlMode.PercentOutput,0);
 		angleMotor.set(ControlMode.PercentOutput,0);
 	}
 
+	/**
+	 * @return returns the target angle of the angular motor
+	 */
 	@Override
 	public double getTargetAngle() {
 		return targetAngle;
 	}
 
+	/**
+	 * @return get the linear motor's target velocity
+	 */
 	@Override
 	public double getTargetVel() {
 		return targetVel;
 	}
 
+	/**
+	 * @return return the @class {@link SwerveModuleState} object of the angle and velocity of the module
+	 */
 	@Override
 	public SwerveModuleState getModuleState() {
 		return new SwerveModuleState(
@@ -146,13 +175,19 @@ public class SdsSwerveModule implements SwerveModule{
 				);
 	}
 
+	/**
+	 * get the magEncoder's angle raw units (analog to digital converter)
+	 * */
 	@Override
 	public double getAbsoluteEncoderValue() {
-		return 0;
+		return magEncoder.getValue();
 	}
 
+	/**
+	 * @param power - double between 1 to -1 to set the percentage of the motors rotation
+	 */
 	@Override
 	public void setRotPowerOnlyForCalibrations(double power) {
-
+		angleMotor.set(ControlMode.PercentOutput, power);
 	}
 }
