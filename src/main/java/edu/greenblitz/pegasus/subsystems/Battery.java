511450843b931602ada70426b61ab689936d25fd
itamaroryan <itamar.oryan@gmail.com>
2022-12-19 17:39:35 +0200

    noam - battery limited tested now working

--- src/main/java/edu/greenblitz/pegasus/subsystems/Battery.java
+++ src/main/java/edu/greenblitz/pegasus/subsystems/Battery.java
@@ -7,7 +7,7 @@ import java.sql.Driver;
 public class Battery extends GBSubsystem {
 
 	private double currentVoltage;
-	private static final double minVoltage = 12.50;
+	private static final double minVoltage = 11.97;
 	private static Battery instance;
 
 
